d3f30aea171acb227b1cc79a242d3b87d88f1908
adamsanderson <netghost@gmail.com>
2013-06-24 23:46:07 -0700

    Clean up annotate example a little bit.

--- examples/annotate.rb
+++ examples/annotate.rb
@@ -4,44 +4,70 @@ $:.unshift(DIR_NAME + '/../lib')
 require "metric_adapter"
 require "./#{DIR_NAME}/analyzer_mixin"
 
+#
+# This is an example of using MetricAdapter to provide a common
+# interface for Flay, Flog, and Reek's metrics to annotate a file.
+#
+
 include AnalyzerMixin
 
-files = ARGV
+COLORS = {
+  :normal    => "\033[39m",
+  :alternate => "\033[38;5;109m",
+  :faded     => "\033[90m"
+}
+
+files = ARGV.empty? ? [__FILE__] : ARGV
 
 def report_on_file(path, metrics)
+  puts "\n#{path}"
   return if metrics.empty?
   
   metrics_by_line = metrics.group_by{|m| m.location.line }
   
   source = IO.read(path)
   source.lines.each_with_index do |src_line, i|
-    line_number = i + 1 # Logical line numbers, not indices
-    src_line    = src_line.chomp # Strip newline
-    indent      = src_line.scan(/\A\s+/).first
-    
-    if metrics_by_line[line_number]
-      metrics_by_line[line_number].each do |m|
-        puts "\033[90m%5d. \033[38;5;109m#{indent}%s\033[0m" % [line_number, m.message]
-      end
-    end
-    
-    if src_line =~ /^\s*#/
-      puts "\033[90m%5d. \033[90m%s\033[0m" % [line_number, src_line]
-    else
-      puts "\033[90m%5d. \033[39m%s\033[0m" % [line_number, src_line]
-    end
+    line_number  = i + 1 # Logical line numbers, not indices
+    src_line     = src_line.chomp # Strip newline
+    indent       = src_line.scan(/\A\s+/).first
+    line_metrics = metrics_by_line[line_number]
 
+    print_metrics(line_number, line_metrics, indent)
+    print_src(line_number, src_line)
   end
+end
+
+def print_metrics(line_number, metrics, indent = "")
+  return unless metrics
+  
+  metrics.each do |m|
+    print_line line_number, "#{indent}#{m.message}", :alternate
+  end
+end
+
+def print_src(line_number, src_line)
+  if src_line =~ /^\s*#/
+    # Print a comment
+    print_line line_number, src_line, :faded
+  else
+    # Print a normal line
+    print_line line_number, src_line, :normal
+  end
+end
+
+def print_line(line_number, text, name)
+  formatted_number = '%5d.' % line_number
+  puts "#{color formatted_number, :faded} #{color text, name}"
+end
+
+def color(text, name)
+  "#{COLORS[name]}#{text}\033[0m"
 end
 
 # Combine metrics from all our sources (See AnalyzerMixin):
 metrics = flay(files, 16) + flog(files) + reek(files)
 
-metrics_by_path = Hash.new{|h,k| h[k] = [] }
-metrics.each do |m|
-  path = m.location.path
-  metrics_by_path[path] << m
-end
+metrics_by_path = metrics.group_by{|m| m.location.path }
 
 files.each do |path|
   metrics = metrics_by_path[path]
